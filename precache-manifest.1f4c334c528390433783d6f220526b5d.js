ee0cd426a382e184536dd89cddae4bca71e71074
Alvaro Augusto Roberto <aroberto@unicamp.br>
2020-02-12 08:10:57 -0300

    Updates

---
+++ precache-manifest.1f4c334c528390433783d6f220526b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799daab3ebdebcc0c0304e8a9c774bfc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae80ac797c960cca3d27",
+    "url": "/monsters-rolodex/static/css/main.b1fcd938.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae80ac797c960cca3d27",
+    "url": "/monsters-rolodex/static/js/main.f47b01ec.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
